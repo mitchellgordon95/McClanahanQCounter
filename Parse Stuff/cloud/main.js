caf0755fd1063926ec984cfde05718e96ccc79d3
Mitchell Gordon <mitchell.gordon95@gmail.com>
2015-09-08 11:41:16 -0500

    Using Parse cloud code instead of directly updating objects

---
+++ Parse Stuff/cloud/main.js
@@ -0,0 +1,32 @@
+Parse.Cloud.define("incrementCounter", function(request, response) {
+     
+    var Counter = Parse.Object.extend("Counter");
+    var query = new Parse.Query(Counter); 
+    query.get("OYMHX9dQV4", {
+        success: function(counter) {
+            if (Date.now() - counter.updatedAt > 30000)
+            {
+                count = counter.get("count") + 1;
+                counter.set("count", count);
+                counter.save(null, {
+                    success: function() {
+                        response.success(count);
+                    },
+                    error: function() {
+                        response.error("Screw you, Mark.");
+                    }
+                });
+                
+            }
+            else
+            {
+                response.error("Screw you, Mark.");
+            }
+        },
+        error: function(object, error) {
+            response.error("Screw you, Mark.");
+        }
+    });
+
+});
+               
